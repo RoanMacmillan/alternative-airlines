d92601a9983139194bd982a479cd6d655fb29d56
RoanMacmillan <hibee94@gmail.com>
2023-07-11 17:09:46 +0100

    added desktop nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,23 @@ const Header = () => {
           src={Logo}
           alt="Alternative Airlines company logo"
         />
-        <img src={Hamburger} alt="Hamburger Button" />
+
+        <nav className="hidden text-customGray w-full text-sm leading-[22px] mr-auto ml-10 gap-10 lg:flex">
+
+        <a>Book flights</a>
+        <a>Information</a>
+        <a>Help</a>
+        <a>Travel Agents</a>
+
+        <a className="ml-auto">Manage booking</a>
+
+
+        </nav>
+
+
+
+
+        <img className="lg:hidden" src={Hamburger} alt="Hamburger Button" />
       </header>
     </>
   );
